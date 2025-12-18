3669c3f8147c783477633ddd2978b8d010acf3a9
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2025-12-19 00:48:04 +0500

    Front side of header section's  half ready

--- FRONT/app/src/App.tsx
+++ FRONT/app/src/App.tsx
@@ -4,11 +4,16 @@ import {
   Flex
 } from "@chakra-ui/react"
 import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+
+  const [isOpenedList,setList] = useState(false)
+
   return ( 
-    <Flex w={'100%'} bg={'black'} h={'100vh'} justifyContent={'center'}>
-       <Box border={'2px solid white'} 
+    <Flex w={'100%'} bg={'black'} h={'100vh'} textOverflow={'hidden'} justifyContent={'center'}>
+       <Box border={'2px solid yellow'} 
+         
         w={{base:"96vw", sm:"70vw",md:"50vw",lg:"40vw", xl:"25vw","2xl":"24vw"}} 
         h={{base:"80vh", sm:"80vh",md:"75vh",xl:"60vh","2xl":"72vh"}} 
         mt={{base:"20vw",sm:"10vh",xl:"10vw","2xl":"6vw"}} 
--- FRONT/app/src/components/Header.tsx
+++ FRONT/app/src/components/Header.tsx
@@ -1,21 +1,32 @@
 import {
     Box,
+    Link,
     Text,
     Flex,
     Button
 } from "@chakra-ui/react"
-import Chevrone from "../Icons/Chevrone";
+// import Chevrone from "../Icons/Chevrone";
 import Menu from "../Icons/Menu";
 import Add from "../Icons/Add";
+import { useState } from "react";
 
 import buttonsBG from "../backgrounds/buttonsBG.jpg"
+  
+
 
 function Header(){
+
+  const [isOpenedList,setList] = useState(true);
+  const [isID,setID] = useState(1)
+
+  
+
       return(
         <>
-        <Box bg={"white"} mt={6} >
-           <Flex justifyContent={"space-between"}>
-             <Button  borderRadius={'full'} 
+        <Box  mt={6}   display={"flex"} flexDir={"column"} gap={2}  >
+           <Flex justifyContent = {"space-between"}>
+             <Button  borderRadius={'full'}  
+              boxShadow={"0 0 170px 9px white"}
               bgImage={`${buttonsBG}`} 
               bgPos={"center"} 
               _hover={{opacity:1}} 
@@ -27,6 +38,7 @@ function Header(){
                 <Add/>
               </Button>
              {/* <Button  borderRadius={'full'} 
+              boxShadow={"0 0 170px 9px white"}
               bgImage={`${buttonsBG}`} 
               bgPos={"center"} 
               _hover={{opacity:1}} 
@@ -39,6 +51,7 @@ function Header(){
                  <Chevrone/>
               </Button> */}
              <Button  borderRadius={'full'} 
+              boxShadow={"0 0 170px 9px white"}
               bgImage={`${buttonsBG}`} 
               bgPos={"center"} 
               _hover={{opacity:1}} 
@@ -46,10 +59,26 @@ function Header(){
               _active={{transform: "scale(0.8)",}} 
               border={"2px solid black"}  
               w={{base:"50px","2xl":"57px"}} 
-              h={{base:"50px", "2xl":"55px"}}>
+              h={{base:"50px", "2xl":"55px"}}
+              onClick={()=>{setList(!isOpenedList)}}
+              >
                  <Menu/>
               </Button>
            </Flex>
+           {isOpenedList 
+           ? <Flex  h={"20vh"} rounded={'20px'} bg={'#241816'} flexDir={'column'} border={"2px solid black"} boxShadow={"0 110px 600px -50px white"}>
+              <Box as="nav" h={"3vh"} w={"100%"}  display={'flex'} justifyContent={"space-between"}>
+                 <Button onClick={()=>{setID(1)}} _hover={{opacity:1}} _active={{opacity:1}}  padding={'none'} borderRadius={'none'}  border={isID ===1 ? '6px double black' : ''} borderBottom={'none'}  roundedTopLeft={'20px'} overflow={'hidden'} ml={isID=== 1 ? '-2px' :'none'} roundedTopRight={'20px'} w={'100%'} bgImage={buttonsBG} bgPos={"bottom"}>
+                  <Link  w={'90vw'}    textDecoration="none" _active={{textDecoration:'none'}}  h={"100%"}   fontSize={"20px"}  display={'flex'} justifyContent={'center'} alignItems={'center'} >All</Link>
+                 </Button>
+                 <Button onClick={()=>{setID(2)}} _hover={{opacity:1}} _active={{opacity:1}}  padding={'none'} borderRadius={'none'}  border={isID === 2 ? '6px double black' : ''} borderBottom={'none'}  roundedTopLeft={ '20px'} overflow={'hidden'} roundedTopRight={'20px'} w={'100%'} mr={isID ===2 ? '-2px' :''} bgImage={buttonsBG} bgPos={"bottom"}>
+                  <Link  w={'90vw'} textAlign={'center'} h={"100%"}  fontSize={"20px"}  display={'flex'} justifyContent={'center'} alignItems={'center'} >Favourited </Link>
+                 </Button>
+              </Box>
+              <Text>a</Text>
+           </Flex>
+           :''
+           }
         </Box>
         </>
       )
